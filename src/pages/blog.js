e06d5857f9b256531042ad028d6acb0f4df8531e
Katharina Schreiber <ms.schreiber.katharina@gmail.com>
2021-04-26 12:02:49 +0200

    blog-placeholder

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -7,10 +7,15 @@ import Navbar from '../partials/Navbar';
 
 function Blog() {
   return (
-    <div className="flex flex-col min-h-screen overflow-hidden">
+    <div className="min-h-screen overflow-hidden">
 		<Navbar />
-      {/*  Site footer */}
-    	<Footer />
+      <div className="max-w-4xl mx-auto py-12 lg:py-16 ">
+        <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+          <span className="block">Coming soon!</span>
+          <span className="block text-indigo-600">I am just learning stuff about headless CMS and will build a blog here with strapi.io. Hang in!</span>
+        </h2>
+      </div>
+    <Footer />
     </div>
   );
 }
